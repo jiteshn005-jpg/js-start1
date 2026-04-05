ec77dffc21596ffaabf7d06d96ce2bda39c206eb
jiteshn005-jpg <jiteshn005@gmail.com>
2026-04-05 07:51:40 +0000

    datatypes

--- basics/basic_01.js
+++ basics/basic_01.js
@@ -1,2 +1,21 @@
+"use strict"; //used to use strictly latest format
 console.log("Jitesh")
 console.log("Bhas")
+
+
+const id=78463
+let name1="JItesh BHaskar"
+let blank
+
+//use let in place of var(for defining variable) always because of its issue in block scope and functional scope
+
+//sur="idk xyz" //not suggested 
+//cant use the above line in latest format
+
+name1="jitesh bhaskar"
+
+console.log(id)
+console.log(name1)
+console.log(id)
+console.log(typeof null)
+console.table([id,name1,blank,3+3,typeof id,typeof name1])
